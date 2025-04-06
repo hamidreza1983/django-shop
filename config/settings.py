db83a826afdd24aa175cd9363ecc17311ecccb1e
hamidreza1983 <hamidreza1983@gmail.com>
2025-04-05 19:10:51 -0700

    edit vulnerability and add database

--- config/settings.py
+++ config/settings.py
@@ -85,22 +85,22 @@ WSGI_APPLICATION = 'config.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/5.1/ref/settings/#databases
 
-# DATABASES = {
-#     'default': {
-#         'ENGINE': 'django.db.backends.sqlite3',
-#         'NAME': BASE_DIR / 'db.sqlite3',
-#     }
-# }
 DATABASES = {
-   'default': {
-       'ENGINE': 'django.db.backends.postgresql',
-       'NAME': os.getenv('POSTGRES_DB', 'mydb'),
-       'USER': os.getenv('POSTGRES_USER', 'myuser'),
-       'PASSWORD': os.getenv('POSTGRES_PASSWORD', 'mypassword'),
-       'HOST': 'posg',
-       'PORT': '5432',
-   }
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
+    }
 }
+# DATABASES = {
+#    'default': {
+#        'ENGINE': 'django.db.backends.postgresql',
+#        'NAME': os.getenv('POSTGRES_DB', 'mydb'),
+#        'USER': os.getenv('POSTGRES_USER', 'myuser'),
+#        'PASSWORD': os.getenv('POSTGRES_PASSWORD', 'mypassword'),
+#        'HOST': 'posg',
+#        'PORT': '5432',
+#    }
+# }
 
 
 
